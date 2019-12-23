67120f1f926a1d12bd64fa259b9e4f23b90c03a9
Rifat-Hossain-Khan <rifat.hk889@gmail.com>
2019-12-23 15:25:57 +0600

    rifat

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/graphql", graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ["Playing", " Sailing", "Cooking"];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000");
+});
